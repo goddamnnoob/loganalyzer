158cca4df020bf327691b257dd70c4b9662b12fb
goddamnnoob <gowthamreddy547@gmail.com>
2022-06-07 22:48:56 +0530

    added unique exceptions file

--- app/UniqueExceptions.go
+++ app/UniqueExceptions.go
@@ -1,7 +1,53 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-func UniqueExceptions(logs *string) {
-	fmt.Println(logs)
+func UniqueExceptions(logsFolderPath *string) {
+	//serverOutFilesCount := 0
+	if !isValidPath(logsFolderPath) {
+		fmt.Println("Invalid Logs Folder Path specified !!!!!!")
+		return
+	}
+	if !isValidDirectory(logsFolderPath) {
+		fmt.Println("Logs Folder Path is not a Directory !!!!!!")
+		return
+	}
+	filesInDirectory := getFilesListInFolder(logsFolderPath)
+	serverOutFilesInDirectory := getServerOutFiles(filesInDirectory)
+	fmt.Println(len(*serverOutFilesInDirectory))
+}
+
+func isValidPath(path *string) bool {
+	_, err := os.Open(*path)
+	return err == nil
+}
+
+func isValidDirectory(path *string) bool {
+	dir, _ := os.Stat(*path)
+	return dir.IsDir()
+}
+
+func getFilesListInFolder(path *string) *[]string {
+	var filesInDirectory []string
+	file, _ := os.Open(*path)
+	filesList, _ := file.Readdir(0)
+	for _, f := range filesList {
+		filesInDirectory = append(filesInDirectory, f.Name())
+	}
+	return &filesInDirectory
+}
+
+func getServerOutFiles(filesInDirectory *[]string) *[]string {
+	serverOut := "serverout"
+	var serverOutFiles []string
+	for _, f := range *filesInDirectory {
+		if strings.Contains(f, serverOut) {
+			serverOutFiles = append(serverOutFiles, f)
+		}
+	}
+	return &serverOutFiles
 }
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"github.com/goddamnnoob/loganalyzer/app"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	str := "/Users/gowtham/Projects/loganalyzer"
+	app.UniqueExceptions(&str)
 }
